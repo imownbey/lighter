a451e135c2adc0e39f2e335058189033ae62a66b
Alex Sexton <alex@pierre.co>
2024-09-03 22:36:44 -0500

    Delete tests again

--- lib/test/browser.test.ts
+++
@@ -1,6 +0,0 @@
-import { highlight, extractAnnotations } from "../dist/browser.esm.mjs";
-import { runTests } from "./highlight.js";
-import { runAnnotationTests } from "./annotations.js";
-
-runTests({ highlight });
-runAnnotationTests({ extractAnnotations, highlight });
--- lib/test/esm.test.ts
+++
@@ -1,6 +0,0 @@
-import { highlight, extractAnnotations } from "../dist/index.esm.mjs";
-import { runAnnotationTests } from "./annotations.js";
-import { runTests } from "./highlight.js";
-
-runTests({ highlight });
-runAnnotationTests({ extractAnnotations, highlight });
